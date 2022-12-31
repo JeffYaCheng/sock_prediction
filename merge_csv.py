40c4f2d6d21ccc2fd270c011de8914739bd606f9
Jeff <e24084127@gs.ncku.edu.tw>
2022-12-31 20:46:00 +0800

    add 2008-2016 exchage rate data

--- merge_csv.py
+++ merge_csv.py
@@ -6,17 +6,17 @@ import matplotlib.pyplot as plt
 files = glob(r'D:\111-1\machine_learning\make_change_rate_data/*.csv')
 print("file length=",len(files))
 df = pd.concat(
-    (pd.read_csv(file, encoding='Big5', usecols=['日期','美元／新台幣'], dtype={ 'name': str, 'tweet':str}) for file in files),ignore_index=True)
+    (pd.read_csv(file, encoding='Big5', usecols=[r'日期',r'美元／新台幣',r'人民幣／新台幣'], dtype={ r'日期': str, r'美元／新台幣':str,r'人民幣／新台幣': str}) for file in files),ignore_index=True)
 df.to_csv(f"exchage_rate.csv")
 print("df shape=",df.shape[0],df.shape[1])
 print(df.head())
 print(df.tail())
-buy_list=list(df.loc[:,'美元／新台幣'])
+buy_list=list(df.loc[:,'人民幣／新台幣'])
 print("buy_list type=",type(buy_list))
 print("buy_list length=",len(buy_list))
 df = df.set_index('日期')  # 指定日期欄位為datafram的index
 df = df.sort_index(ascending=True)
 plt.figure(figsize=(30, 8))
-df['美元／新台幣'].plot()  # x=['date'], y=[['buy_rate','sell_rate']] 
+df['人民幣／新台幣'].plot()  # x=['date'], y=[['buy_rate','sell_rate']] 
 plt.legend(loc="upper left")
 plt.show()
